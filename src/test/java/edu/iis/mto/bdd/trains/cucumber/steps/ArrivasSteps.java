a9dccfc89aeec81ea2c1eb9cecb27d4834d2b45c
Caroolinnee <karolinab341@gmail.com>
2019-06-23 17:06:33 +0200

    feature2_rev0

---
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/ArrivasSteps.java
@@ -0,0 +1,41 @@
+package edu.iis.mto.bdd.trains.cucumber.steps;
+
+import org.joda.time.LocalTime;
+
+import cucumber.api.PendingException;
+import cucumber.api.Transform;
+import cucumber.api.java.pl.Gdy;
+import cucumber.api.java.pl.I;
+import cucumber.api.java.pl.Wtedy;
+import cucumber.api.java.pl.Zakładając;
+
+public class ArrivasSteps
+    {
+
+    @Zakładając("^chcę się dostać z \"([^\"]*)\" do \"([^\"]*)\"$")
+    public void chcęSięDostaćZDo(String departure, String destination) throws Throwable
+        {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+        }
+
+    @I("^następny pociąg odjeżdża o \"([^\"]*)\" na linii \"([^\"]*)\"$")
+    public void następnyPociągOdjeżdżaONaLinii(String arg0,
+                                               @Transform(JodaLocalTimeConverter.class) LocalTime time) throws Throwable
+        {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+        }
+
+    @Gdy("^zapytam o godzinę przyjazdu$")
+    public void zapytamOGodzinęPrzyjazdu()
+        {
+        }
+
+    @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: \"([^\"]*)\"$")
+    public void powinienemUzyskaćNastępującySzacowanyCzasPrzyjazdu(@Transform(JodaLocalTimeConverter.class) LocalTime time) throws Throwable
+        {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+        }
+    }
